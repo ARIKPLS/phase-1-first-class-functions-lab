0bc352c3ec2befb080ba47f70b7487347ee8dfdb
EricVentura <ericvent@outlook.com>
2023-04-08 16:44:26 -0400

    in progress

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function firstTwo([]){
+    return (['Antonia', 'Nuru']);
+}
+
+const returnLastTwoDrivers = function lastTwo([]){
+    return (['Amari', 'Mo']);
+}
