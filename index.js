9c5dc593a435bec0d72e3dd8290029c9f8d7f67b
EricVentura <ericvent@outlook.com>
2023-04-08 21:19:24 -0400

    test pass

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const returnFirstTwoDrivers = array => array.slice(0,2)
 
 const returnLastTwoDrivers = array => array.slice(-2)
 
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+const createFareMultiplier = (num1) => function (fare){return num1 * fare}
+
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = (arrayOfDrivers, funk) => {
+    return funk(arrayOfDrivers)
+} 
+
